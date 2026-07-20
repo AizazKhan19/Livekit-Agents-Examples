83104e9778e3e35df4d57b3633adc9411decb58f
Aizaz Khan <aizazkhan7874@gmail.com>
2026-07-20 10:28:47 +0500

    uninterruptable agent created

---
+++ uninterruptable/uninterruptable.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from livekit.agents import JobContext, JobProcess, AgentServer, cli, Agent, AgentSession, inference
+from livekit.plugins import silero
+
+load_dotenv()
+
+class UninterruptableAgent(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions="""
+                You are a helpful assistant communicating through voice who is not interruptable.
+            """,
+            allow_interruptions=False
+        )
+
+    async def on_enter(self):
+        self.session.generate_reply(user_input="Say something somewhat long and boring so I can test if you're interruptable.")
+
+server = AgentServer()
+
+def prewarm(proc: JobProcess):
+    proc.userdata["vad"] = silero.VAD.load()
+
+server.setup_fnc = prewarm
+
+@server.rtc_session()
+async def entrypoint(ctx: JobContext):
+    ctx.log_context_fields = {"room": ctx.room.name}
+
+    session = AgentSession(
+        stt=inference.STT(model="deepgram/nova-3-general"),
+        llm=inference.LLM(model="openai/gpt-4.1-mini"),
+        tts=inference.TTS(model="cartesia/sonic-3", voice="9626c31c-bec5-4cca-baa8-f8ba9e84c8bc"),
+        vad=ctx.proc.userdata["vad"],
+        preemptive_generation=True,
+    )
+
+    await session.start(agent=UninterruptableAgent(), room=ctx.room)
+    await ctx.connect()
+
+if __name__ == "__main__":
+    cli.run_app(server)
